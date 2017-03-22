d7ba8150f34eadcb9a30283805b0f2888fa43911
Spencer Gardner <spencergardner@gmail.com>
2017-03-22 18:12:46 -0500

    finish merge

--- neighborhood-lcd.py
+++ neighborhood-lcd.py
@@ -127,7 +127,6 @@ def main(argv):
     # get improved minimum cost
     minCostArrayA = costMinArray(costSurfaceArrayA,inputRadius)
     minCostArrayB = costMinArray(costSurfaceArrayB,inputRadius)
-<<<<<<< HEAD
     imprvMinCostArray = np.add(       # add constant equal to 2xradius to represent new connection
         minCostArrayA,
         minCostArrayB,
