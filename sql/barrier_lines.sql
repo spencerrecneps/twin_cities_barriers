669ae279c144dae2707270110d1d621ded6c1599
Spencer Gardner <spencergardner@gmail.com>
2017-04-25 08:06:40 -0500

    Continued experimentation.

--- sql/barrier_lines.sql
+++ sql/barrier_lines.sql
@@ -5,26 +5,38 @@
 --   i.e. psql -v db_srid=26915
 ------------------------------------------------------------
 -- create table
+DROP TABLE IF EXISTS scratch.barrier_pointsdump;
+CREATE TABLE scratch.barrier_pointsdump (
+    id SERIAL PRIMARY KEY,
+    feat INTEGER,
+    pt INTEGER,
+    geom geometry(point,:db_srid)
+);
+
 DROP TABLE IF EXISTS barrier_lines;
 CREATE TABLE automated.barrier_lines (
     id SERIAL PRIMARY KEY,
     geom geometry(multilinestring,:db_srid)
 );
 
-INSERT INTO automated.barrier_lines (geom)
-SELECT ST_ApproximateMedialAxis(geom)
-FROM scratch.barrier_polys;
+INSERT INTO scratch.barrier_pointsdump (feat, pt, geom)
+SELECT (dump).path[1], (dump).path[2], (dump).geom
+FROM (SELECT ST_DumpPoints(ST_ApproximateMedialAxis(geom)) AS dump FROM scratch.barrier_polys) a;
 
--- UPDATE  automated.barrier_lines
--- SET     geom = ST_Simplify(geom,60,True);
+
+WITH RECURSIVE 
+
+
+INSERT INTO automated.barrier_lines (geom)
+
+from
+(select id, st_dumppoints(geom) as geom from barrier_lines limit 100) a
+
+
+
+
+
 
 -- index
 CREATE INDEX sidx_barrier_lines_geom ON automated.barrier_lines USING GIST (geom);
 ANALYZE automated.barrier_lines;
-
-
-create table scratch.tmp_i (
-    id SERIAL PRIMARY KEY,
-    geom geometry(multilinestring,:db_srid)
-);
-insert into tmp_i (geom) select st_segmentize(geom,100) from barrier_lines;
