ea8db49c09457efed3f8b56e0e50ba3d6a01fb4c
Spencer <spencergardner@gmail.com>
2017-07-06 12:42:04 -0500

    Add point type to barrier results

--- sql/barrier_points.sql
+++ sql/barrier_points.sql
@@ -9,13 +9,15 @@ DROP TABLE IF EXISTS barrier_points;
 CREATE TABLE automated.barrier_points (
     id SERIAL PRIMARY KEY,
     geom geometry(point,:db_srid),
-    community_type TEXT
+    community_type TEXT,
+    point_type TEXT
 );
 
 -- insert
-INSERT INTO automated.barrier_points (geom, community_type)
+INSERT INTO automated.barrier_points (geom, community_type, point_type)
 SELECT  ST_Centroid(geom),
-        community_type
+        community_type,
+        point_type
 FROM    barrier_deviation_test_lines
 WHERE   cost_exist >    CASE
                         WHEN community_type = 'urban center' THEN 600
